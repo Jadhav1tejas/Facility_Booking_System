244521c0587185e411b16d75f4a2a5c2101f1735
Jadhav1tejas <tejas1610jadhav@gmail.com>
2026-07-11 12:11:29 +0530

    Update default admin credentials

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='Admin@Pixa').exists() or User.objects.create_superuser('Admin@Pixa', 'admin@example.com', 'AdminPixa@2026')"
 
