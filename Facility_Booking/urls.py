5aa715af1a535613b088b5b924164bfcc9ca474d
Jadhav1tejas <tejas1610jadhav@gmail.com>
2026-07-11 12:12:41 +0530

    Change admin URL to pixa-admin-portal for security

--- Facility_Booking/urls.py
+++ Facility_Booking/urls.py
@@ -17,6 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('pixa-admin-portal/', admin.site.urls),
     path('', include('Booking.urls')),
 ]
